ae03f456f1953d36600a8630931784868d579a9d
kelvin <hope_kelvin@163.com>
2017-03-07 20:15:38 +0800

    取消meta的注释

--- server/app.js
+++ server/app.js
@@ -1,10 +1,26 @@
 var express = require("express");
 var request = require("request");
+var fs = require("fs");
 var http = require("http");
+var https = require("https");
+var privateKey = fs.readFileSync("/var/local/ssl/private.pem","utf8");
+var certificate = fs.readFileSync("/var/local/ssl/file.crt","utf8");
+var credentials = {key : privateKey,cert : certificate};
 
 var app = express();
+var httpServer = http.createServer(app);
+var httpsServer = https.createServer(credentials,app);
+var PORT = 8080;
+var SSLPORT = 8090;
 
-var port = 8090;
+httpServer.listen(PORT,function(){
+  console.log("HTTP Server is running on : http://localhost:%s",PORT);
+});
+
+httpsServer.listen(SSLPORT,function(){
+  console.log("HTTPS Server is running on : https://localhost:%s",SSLPORT);
+});
+
 var requestUrl = {
   "cloud_music_api" : "https://music.163.com/api/playlist/detail?id=1612159"
 };
@@ -12,16 +28,28 @@ var outerRequest = {
   "request_music" : "/get_music_list"
 };
 
-app.listen(port,function(){
-  console.log("listening on port " + port);
-});
-
-// 请求网易云的api,获取数据
+// app.listen(port,function(){
+//   console.log("listening on port " + port);
+// });
+//
+// // 请求网易云的api,获取数据
 app.get(outerRequest.request_music,function(req,res){
-  console.log("请求网易云音乐列表!");
-  // console.log(requestMusicList(requestUrl.cloud_music_api));
-  res.send(requestMusicList(requestUrl.cloud_music_api));
+  if(req.protocol === 'https'){
+    // res.status(200).send("Welcome to Safety Land!");
+    console.log("请求网易云音乐列表!");
+    // console.log(requestMusicList(requestUrl.cloud_music_api));
+    res.send(requestMusicList(requestUrl.cloud_music_api));
+  }else{
+    res.status(200).send("Welcome");
+  }
 })
+
+// 接收https请求
+// https.get(outerRequest.request_music,function(res){
+//   console.log('statusCode:',res.statusCode);
+//   console.log('headers:',res.headers);
+//
+// });
 
 // 用http来接收请求，并使用 request和它的 pipe()方法
 // http.createServer(function(req,resp){
