d9f4758997f9d4f71ec72aa2a3f8e1d162eed722
root <root@localhost.localdomain>
2017-03-10 03:12:17 -0500

    修改

--- public/scripts/bundle.js
+++ public/scripts/bundle.js
@@ -63,14 +63,22 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ 	return __webpack_require__(__webpack_require__.s = 1);
 /******/ })
 /************************************************************************/
 /******/ ([
 /* 0 */
 /***/ (function(module, exports) {
 
+module.exports = 'It works from module.js';
+
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports, __webpack_require__) {
+
 document.write("It works.");
+document.write(__webpack_require__(0));// 添加新的模块
 
 
 /***/ })
