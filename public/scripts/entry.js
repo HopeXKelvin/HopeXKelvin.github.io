7082323199da767fcfc263e697b63d51974fb7d4
LuoKaiHui(桌面事业部_语音交互技术中心) <luokaihui@sogou-inc.com>
2017-03-10 16:17:57 +0800

    删减

--- public/scripts/entry.js
+++ public/scripts/entry.js
@@ -1,5 +1,3 @@
 require("!style-loader!css-loader!./../styles/css/reset.css")// 载入 reset.css
 document.write("It works.");
 document.write(require('./module.js'));// 添加新的模块
-document.write("It works.");
-document.write(require('./module.js'));// 添加新的模块
