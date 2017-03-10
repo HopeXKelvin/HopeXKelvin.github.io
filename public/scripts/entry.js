587629ca0655c4eb9ba9254afa070b35d4aff8c7
LuoKaiHui(桌面事业部_语音交互技术中心) <luokaihui@sogou-inc.com>
2017-03-10 16:43:00 +0800

    新增webpack 位置文件

--- public/scripts/entry.js
+++ public/scripts/entry.js
@@ -1,3 +1,3 @@
-require("!style-loader!css-loader!./../styles/css/reset.css")// 载入 reset.css
+require("./../styles/css/reset.css")// 载入 reset.css
 document.write("It works.");
 document.write(require('./module.js'));// 添加新的模块
