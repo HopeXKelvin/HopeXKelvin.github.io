a3104b21908b3406daa579b1e7925e6866c68567
LuoKaiHui(桌面事业部_语音交互技术中心) <luokaihui@sogou-inc.com>
2017-03-15 15:57:27 +0800

    新修改

--- public/scripts/entry.js
+++ public/scripts/entry.js
@@ -1,13 +1,7 @@
 // javascript
-import bar from './bar';
+import bar from './module';
 
 bar();
-
-let People = require('./module.js');
-// let p = new People("Kelvin");
-console.log(People);
-// p.sayHi();
-// document.write("It works.");
 
 // css
 require("./../styles/sass/index.scss")
