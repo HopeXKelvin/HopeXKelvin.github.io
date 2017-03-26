ea945cf74a17898334bae0e6ff45d22685bab8b4
kelvin <hope_kelvin@163.com>
2017-03-26 19:51:51 +0800

    修改webpack配置参数

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 var webpack = require('webpack');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
   entry : './public/scripts/entry.js',
@@ -7,12 +8,22 @@ module.exports = {
     filename : './public/scripts/bundle.js'
   },
   module : {
+    rules : [
+      {
+        test : /\.css$/,
+        use : ExtractTextPlugin.extract({
+          use : 'css-loader'
+        })
+      }
+    ],
     loaders : [
       {test : /\.css$/,loader : 'style-loader!css-loader'},
-      {test : /\.scss$/,loader:"style-loader!css-loader!sass-loader"}
+      {test : /\.scss$/,loader:"style-loader!css-loader!sass-loader"},
+      {test : /\.(jpg|png|gif)$/,}
     ]
   },
   plugins : [
-    new webpack.BannerPlugin('This file is create by kelvin!')
+    new webpack.BannerPlugin('This file is create by kelvin!'),
+    new ExtractTextPlugin('styles.css')
   ]
 };
