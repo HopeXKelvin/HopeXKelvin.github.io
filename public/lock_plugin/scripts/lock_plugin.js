fc944be62472dd038dcc6a94aed390723357eaf8
kelvin <hope_kelvin@163.com>
2017-03-27 16:23:32 +0800

    1.增加 canvas画线，坐标设置有点出入需要实际在移动端设备上查看

--- public/lock_plugin/scripts/lock_plugin.js
+++ public/lock_plugin/scripts/lock_plugin.js
@@ -1,11 +1,13 @@
 window.onload = function(){
   // 定义一些全局的变量
   var posMap = {};// 存放9个点的x,y轴坐标范围信息,用于后面滑动屏幕解锁的时候做检测
-  var pwdGestureOrder = null;// 存放收拾密码的序列
+  var pwdGestureOrder = [];// 存放收拾密码的序列
+  var lineCanvas = null;
 
   function init(){
     // 初始化方法
     savePointInfo();
+    renderCanvas();
     bindEvent();
   }
 
@@ -14,16 +16,33 @@ window.onload = function(){
     var points = document.getElementsByClassName("lock-item");
     for(var i=0;i<points.length;i++){
       posMap[i+1] = {};
+      // x轴的范围
       var xLeft = points[i].offsetLeft;
       var xRight = points[i].offsetLeft + points[i].clientWidth;
+      // y轴的范围
       var yTop = points[i].offsetTop;
       var yBottom = points[i].offsetTop + points[i].clientHeight;
+      // 圆点的中心坐标
+      var xCenter = xLeft + (xRight-xLeft)/2;
+      var yCenter = yTop + (yBottom-yTop)/2;
       posMap[i+1].x = [xLeft,xRight];
       posMap[i+1].y = [yTop,yBottom];
+      posMap[i+1].pos = [xCenter,yCenter];
       posMap[i+1].hasTap = false;
       posMap[i+1].id = "dot_" + (i+1);
     }
     console.log(posMap);
+  }
+
+  function renderCanvas(){
+    // 渲染画布，画布的作用是画线，生成手势之间的连线
+    var canvas = document.createElement("canvas");
+    canvas.className = "line-canvas";
+    document.body.appendChild(canvas)
+    lineCanvas = canvas.getContext("2d");
+    // lineCanvas.moveTo(0,0)
+    // lineCanvas.lineTo(489,346)
+    // lineCanvas.stroke()
   }
 
   function bindEvent(){
@@ -46,6 +65,16 @@ window.onload = function(){
     });
     lockBox.addEventListener("touchend",function(event){
       console.log("touch end");
+      // 隔个300毫秒，去掉红点效果
+      var selectedPoints = document.getElementsByClassName("selected");
+      setTimeout(function(){
+        while(selectedPoints.length > 0){
+          var index = selectedPoints[0].getAttribute("data-index");//这个一定要放在前面，因为 selectedPoints这个值是动态变化的
+          selectedPoints[0].className = selectedPoints[0].className.replace("selected","");
+          // 还要修改以下posMap的数据结构
+          posMap[index].hasTap = false;
+        }
+      },300);
       console.log(event);
     });
   }
@@ -62,6 +91,18 @@ window.onload = function(){
         if((xpos>xL && xpos<xR) && (ypos>yT && ypos<yB)){
           // 检测该点在触碰的范围内
           console.log("第 " + index + "个点被碰到了");
+          if(pwdGestureOrder.length <= 0){
+            // 表示数组为空
+          }else{
+            // 前面已经有序列了
+            var prevIndex = pwdGestureOrder[pwdGestureOrder.length-1];
+            var prevPos = posMap[prevIndex].pos;
+            var nextPos = posMap[index].pos;
+            lineCanvas.moveTo(prevPos[0],prevPos[1]);
+            lineCanvas.lineTo(nextPos[0],nextPos[1]);
+            lineCanvas.stroke();
+          }
+          pwdGestureOrder.push(index);// 把这个点 push 到数组里面
           posMap[index].hasTap = true;
           // 给该原点添加一个类，转变样式
           var tapDom = document.getElementById(posMap[index].id);
@@ -69,6 +110,11 @@ window.onload = function(){
         }
       }
     }
+  }
+
+  // 画线的方法
+  function drawLine(from,to){
+
   }
   // 调用方法
   init();
