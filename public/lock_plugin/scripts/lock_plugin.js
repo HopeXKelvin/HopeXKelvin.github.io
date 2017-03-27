0e3ed6422aedce18c4252d17bf9950fe755c9997
kelvin <hope_kelvin@163.com>
2017-03-27 13:18:41 +0800

    1.添加页面的辅助信息显示，用于调试打印触摸信息 2.添加移动设备的触摸事件

--- public/lock_plugin/scripts/lock_plugin.js
+++ public/lock_plugin/scripts/lock_plugin.js
@@ -1,1 +1,29 @@
-window.onload = function(){};
+window.onload = function(){
+  init();
+
+  function init(){
+    // 初始化方法
+    bindEvent();
+  }
+
+  function bindEvent(){
+    // 定义一些变量
+    var helpDom = document.getElementsByClassName("help-display")[0];
+    // 绑定事件方法
+    var lockBox = document.getElementById("lockBox");
+    lockBox.addEventListener("touchstart",function(event){
+      console.log("touch start !");
+      console.log(event);
+    });
+    lockBox.addEventListener("touchmove",function(event){
+      event.preventDefault();// 阻止页面滚动
+      console.log("touch move");
+      helpDom.innerHTML += "<br>ClientX : " + event.changedTouches[0].clientX + ",ClientY : " + event.changedTouches[0].clientY;
+      console.log(event);
+    });
+    lockBox.addEventListener("touchend",function(event){
+      console.log("touch end");
+      console.log(event);
+    });
+  }
+};
