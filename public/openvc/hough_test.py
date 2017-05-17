972565528240e801b964b5335d3d39f46c0ffa89
Hope_Kelvin <hope_kelvin@163.com>
2017-05-17 19:37:23 +0800

    hough lines 2

--- public/openvc/hough_test.py
+++ public/openvc/hough_test.py
@@ -3,9 +3,9 @@ import numpy as np
 
 img = cv2.imread('games.jpg')
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-edges = cv2.Canny(gray, 50, 150,apertureSize = 3)
+edges = cv2.Canny(gray, 100, 200,apertureSize = 3)
 
-cv2.imwrite('games-edges.jpg',edges)
+# cv2.imwrite('games-edges.jpg',edges)
 
 lines = cv2.HoughLines(edges,1,np.pi/180,100)
 print lines
